a401e6486e0d1cb3d22d1529668ab372ebc5858f
ikjo93 <auddlr100@naver.com>
2022-06-15 01:16:09 +0900

    init : 스프링 부트 프로젝트 초기화

---
+++ src/main/java/ikjo/oauthexample/OauthExampleApplication.java
@@ -0,0 +1,13 @@
+package ikjo.oauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ikjo/oauthexample/OauthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ikjo.oauthexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
